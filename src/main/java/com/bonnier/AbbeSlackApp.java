ab17d3aa1c9933be9851fbf278487b4d8a397b05
Kristina Nylander <kristina.nylander@mittmedia.se>
2021-02-05 15:57:46 +0100

    Replying to messages

--- src/main/java/com/bonnier/AbbeSlackApp.java
+++ src/main/java/com/bonnier/AbbeSlackApp.java
@@ -2,11 +2,18 @@ package com.bonnier;
 
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.jetty.SlackAppServer;
+import com.slack.api.methods.request.auth.AuthTestRequest;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+import com.slack.api.methods.response.chat.ChatUpdateResponse;
+import com.slack.api.model.block.DividerBlock;
+import com.slack.api.model.block.LayoutBlock;
+import com.slack.api.model.block.RichTextBlock;
 import com.slack.api.model.event.MessageEvent;
 import com.slack.api.model.event.ReactionAddedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class AbbeSlackApp {
     public static void main(String[] args) throws Exception {
@@ -20,7 +27,25 @@ public class AbbeSlackApp {
 
         app.event(MessageEvent.class, ((messagePayload, eventContext) -> {
             String text = messagePayload.getEvent().getText();
-            logger.info("MessageText = "+text);
+            logger.info("Token: " + messagePayload.getToken());
+            logger.info("Channel: " + messagePayload.getEvent().getChannel());
+            logger.info("TS: " + messagePayload.getEvent().getTs());
+            //eventContext.client().authTest(a -> a.token(messagePayload.getToken()));
+            final ChatUpdateResponse r = eventContext.client().chatUpdate(u ->
+                    u.channel(messagePayload.getEvent().getChannel())
+                            .text(messagePayload.getEvent().getText() + " och lite mer text!")
+                            .token(System.getenv("SLACK_BOT_TOKEN"))
+                            .user(messagePayload.getEvent().getUser())
+                            .ts(messagePayload.getEvent().getTs())
+            );
+            logger.info(r.getError());
+            ChatPostMessageResponse message = eventContext.client().chatPostMessage(rs -> rs
+                    .channel(messagePayload.getEvent().getChannel())
+                    .threadTs(messagePayload.getEvent().getTs())
+                    .blocks(List.of(new DividerBlock())));
+                    //.text("Här är ett svar!"));
+            logger.info(message.getError());
+            logger.info("MessageText = " + text);
             logger.info(messagePayload.getEvent().getChannel());
             return eventContext.ack();
         }));
